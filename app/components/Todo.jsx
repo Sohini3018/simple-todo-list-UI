9de2bec4bade2d8930324d6f5f70af73a2acc431
Xron Trix <xrontrix@gmail.com>
2023-10-12 02:29:44 +0530

    several changes to improve ui and ux

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,11 +1,26 @@
 "use client";
 import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPencil, faTrash } from "@fortawesome/free-solid-svg-icons";
+export const maxTitle = 30;
 
-const Todo = ({ task, index, handleDelete,handleEdit }) => {
+const Todo = ({ task, index, handleDelete, handleEdit }) => {
   const [isDone, setIsDone] = useState(false);
   const [toggleEdit, setToggleEdit] = useState(false);
+  const [isDetailsVisible, setDetailsVisible] = useState(false);
   const [editedTitle, setEditedTitle] = useState(task.title);
   const [editedDesc, setEditedDesc] = useState(task.desc);
+
+  const handleTitle = (e) => {
+    const title_ = e.target.value;
+    if (title_.length <= maxTitle) {
+      setEditedTitle(title_);
+    }
+  };
+
+  const toggleDetails = () => {
+    setDetailsVisible(!isDetailsVisible);
+  };
 
   const handletaskDone = () => {
     setIsDone(!isDone);
@@ -13,75 +28,102 @@ const Todo = ({ task, index, handleDelete,handleEdit }) => {
 
   const handleToggleEdit = () => {
     setToggleEdit(true);
-  }
+  };
 
-  const handleSaveEdit = ()=>{
-    handleEdit(index,editedTitle,editedDesc);
+  const handleSaveEdit = () => {
+    handleEdit(index, editedTitle, editedDesc);
     setToggleEdit(false);
-  }
+  };
 
   return (
-    <li className="flex flex-grow justify-between items-center bg-[#F4F2FF] border-0 rounded-lg m-5 p-3 flex-col gap-4 md:flex-row md:flex-grow">
+    <li className="flex flex-grow justify-between w-full md:w-auto md:min-w-[500px] max-w-2xl items-center bg-[#F4F2FF] rounded-lg px-5 py-3 my-5 md:mx-8 gap-4 md:flex-row md:flex-grow shadow-lg shadow-black/30">
       <div className="flex flex-col">
-      {toggleEdit? (
-        <div className="flex flex-col gap-3 mt-3">
-          <input
-            type="text"
-            placeholder="Edit the title"
-            className="bg-[#F4F2FF] p-2 border-2 border-gray-400 rounded-lg"
-            value={editedTitle}
-            onChange={(e) => setEditedTitle(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Edit the description"
-            className="bg-[#F4F2FF] p-2 border-2 border-gray-400 rounded-lg"
-            value={editedDesc}
-            onChange={(e) => setEditedDesc(e.target.value)}
-          />
-          <button
-            className="text-white bg-green-400 hover:bg-green-700 rounded-md p-2"
-            onClick={handleSaveEdit}
-          >
-            Save
-          </button>
-        </div>
-      ):(
-        <>
-        <p className={`text-lg font-bold ${isDone ? "line-through" : ""}`}>
-          {task.title}
-        </p>
-        <p className="text-md text-blue-800">{task.desc}</p></>
-      )
-      }
-        
+        {toggleEdit ? (
+          <div className="flex flex-col gap-3 mt-3 w-[62vw] md:w-[40vw]">
+            <input
+              type="text"
+              placeholder="Edit the title"
+              className="bg-[#F4F2FF]  p-2 border-2 border-[#2B1887] rounded-lg"
+              value={editedTitle}
+              onChange={handleTitle}
+            />
+            <textarea
+              rows={3}
+              placeholder="Edit the description"
+              className="bg-[#F4F2FF]  p-2 border-2 border-[#2B1887] rounded-lg"
+              value={editedDesc}
+              onChange={(e) => setEditedDesc(e.target.value)}
+            />
+            <button
+              className="text-white bg-green-400 hover:bg-green-700 rounded-md p-2"
+              onClick={handleSaveEdit}
+            >
+              Save
+            </button>
+          </div>
+        ) : (
+          <div className="flex flex-col">
+            <div className="flex flex-row items-center gap-3">
+              <input
+                type="checkbox"
+                id="done"
+                name="title"
+                className="h-5 w-5 rounded-lg"
+                onClick={() => {
+                  handletaskDone(index);
+                }}
+              />
+              <label
+                for="title"
+                className={`md:text-xl font-bold ${
+                  isDone ? "line-through" : ""
+                }`}
+              >
+                {task.title}
+              </label>
+              <button
+                className={`text-pink-400 text-sm ${
+                  isDetailsVisible ? "hidden" : "block"
+                }`}
+                onClick={toggleDetails}
+              >
+                Show Details
+              </button>
+              <button
+                className={`text-pink-400 text-sm ${
+                  !isDetailsVisible ? "hidden" : "block"
+                }`}
+                onClick={toggleDetails}
+              >
+                Hide Details
+              </button>
+            </div>
+            {isDetailsVisible ? (
+              <p className="mt-4 text-sm text-justify px-8">{task.desc}</p>
+            ) : (
+              <p></p>
+            )}
+          </div>
+        )}
       </div>
 
-      <div className="flex justify-between gap-4">
+      {isDone ? (
         <button
-          className="text-white bg-[#ecb800] hover:bg-yellow-700 rounded-md p-2"
-          onClick={() => {
-            handletaskDone(index);
-          }}
-        >
-          Done
-        </button>
-        <button
-          className="text-white bg-[#ea4863] hover:bg-red-600 rounded-md p-2"
+          className="text-white rounded-md p-2"
           onClick={() => {
             handleDelete(index);
           }}
         >
-          Delete
+          <FontAwesomeIcon className="text-red-600" icon={faTrash} />
         </button>
+      ) : (
         <button
-          className="text-white bg-[#6f87e8] hover:bg-blue-600 rounded-md p-2"
+          className="text-white rounded-md p-2"
           onClick={handleToggleEdit}
         >
-          Edit
+          <FontAwesomeIcon className="text-yellow-600" icon={faPencil} />
         </button>
-        
-      </div>
+      )}
     </li>
   );
 };
--- app/page.js
+++ app/page.js
@@ -1,17 +1,21 @@
 "use client";
+
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAdd, faClipboard } from "@fortawesome/free-solid-svg-icons";
 import Todo from "./components/Todo";
+import { maxTitle } from "./components/Todo";
 
 const page = () => {
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
-
   const [addTask, setAddTask] = useState([]);
 
   const handleTitle = (e) => {
-    setTitle(e.target.value);
+    const title_ = e.target.value;
+    if (title_.length <= maxTitle) {
+      setTitle(title_);
+    }
   };
 
   const handleDesc = (e) => {
@@ -19,13 +23,9 @@ const page = () => {
   };
 
   const handleSubmitTask = () => {
-    {
-      if (title !=="") {
-        setAddTask([...addTask, { title: title, desc: desc }]);
-        setTitle("");
-        setDesc("");
-      } else alert("Please enter a task");
-    }
+    setAddTask([...addTask, { title: title, desc: desc }]);
+    setTitle("");
+    setDesc("");
   };
 
   const handleDelete = (i) => {
@@ -35,55 +35,65 @@ const page = () => {
     setAddTask(updatedTask);
   };
 
-  const handleEdit = (index,editedTitle,editedDesc)=>{
+  const handleEdit = (index, editedTitle, editedDesc) => {
     const updatedTask = [...addTask];
-    updatedTask[index] = {title: editedTitle, desc: editedDesc};
+    updatedTask[index] = { title: editedTitle, desc: editedDesc };
     setAddTask(updatedTask);
   }
 
   return (
-    <div className="h-screen bg-orange-100 py-10">
-    <div className="flex justify-center">
-      <div className="bg-[#D5CCFF] flex flex-col justify-center items-center w-[80%] p-10 border-0 rounded-xl">
-        <p className="w-15 text-2xl text-[#2B1887] font-bold">
-          Todo List <FontAwesomeIcon icon={faClipboard} />
-        </p>
-        <div className="flex justify-center items-center m-5 flex-col gap-3 md:flex-row ">
-          <input
-            type="text"
-            placeholder="Add the title"
-            className="bg-[#F4F2FF] p-3 border-2 border-gray-400 rounded-lg"
-            value={title}
-            onChange={handleTitle}
-          />
-          <input
-            type="text"
-            placeholder="Add some description"
-            className="bg-[#F4F2FF] p-3 border-2 border-gray-400 rounded-lg"
-            value={desc}
-            onChange={handleDesc}
-          />
-          <button
-            className="flex justify-between items-center text-white bg-pink-400 hover:bg-pink-900 rounded-md p-2 w-[7rem]"
-            onClick={handleSubmitTask}
-          >
-            <FontAwesomeIcon icon={faAdd} />
-            Add Task
-          </button>
-        </div>
-        {/* showTasks if there is atleast one task available */}
-        {addTask.length == 0 ? (
-          "No tasks available"
-        ) : (
-          <div>
-            {addTask.map((task, i) => {
-              return <Todo task={task} index={i} handleDelete={handleDelete} handleEdit={handleEdit}/>;
-            })}
+    <main className="h-screen bg-orange-100 py-10 overflow-x-hidden">
+      <div className="flex justify-center">
+        <div className="flex flex-col justify-center items-center w-[80%] py-10 border-0 rounded-xl">
+          <div className="flex flex-row gap-4 items-center mb-6">
+            <p className="w-15 text-4xl text-[#2B1887] font-bold">Todo List</p>
+            <FontAwesomeIcon icon={faClipboard} className="text-4xl text-[#2B1887]" />
           </div>
-        )}
+
+          <div className="flex justify-center items-center m-5 flex-col gap-3 md:flex-row w-[70vw] rounded-lg py-4">
+            <input
+              type="text"
+              placeholder="Add the title"
+              className={`bg-[#F4F2FF] w-64 md:w-auto p-3 border-2 rounded-lg ${title.length === maxTitle ? "text-red-500" : ""} focus:outline-none shadow-lg shadow-black/30 focus:shadow-pink-500`}
+              value={title}
+              onChange={handleTitle}
+            />
+            <input
+              type="text"
+              placeholder="Add some description"
+              className="bg-[#F4F2FF] w-64 md:w-auto p-3 border-2 rounded-lg focus:outline-none shadow-lg shadow-black/30 focus:shadow-pink-500"
+              value={desc}
+              onChange={handleDesc}
+            />
+
+            <button
+              className={`min-w-fit flex justify-between items-center gap-3 text-white ${title.length === 0 ? "bg-gray-400" : "bg-pink-500 hover:bg-pink-700 "} shadow-lg shadow-black/30 rounded-md p-3 w-[7rem] mt-4 md:mt-0`}
+              onClick={handleSubmitTask}
+              disabled={title.length === 0 ? true : false}
+            >
+              <FontAwesomeIcon icon={faAdd} />
+              Add Task
+            </button>
+          </div>
+
+          {/* showTasks if there is at least one task available */}
+          <div className="rounded-lg py-8 w-full md:w-auto md:px-6 md:bg-pink-400 mt-6">
+            {addTask.length == 0 ? (
+              <p className="text-center text-pink-500 md:text-white font-bold">Your Tasks will appear here...</p>
+            ) : (
+              <div>
+                <h3 className="text-center text-xl text-pink-500 md:text-white font-bold mb-8">All Category Tasks</h3>
+                <ul>
+                  {addTask.map((task, i) => {
+                    return <Todo task={task} index={i} handleDelete={handleDelete} handleEdit={handleEdit} />;
+                  })}
+                </ul>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
+    </main>
   );
 };
 
